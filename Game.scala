ae5a51fca270596e847ff17f2643b4af5640f12c
Laurent Raufaste <analogue@glop.org>
2011-09-29 17:31:35 -0400

    Added basic game logic based on rules
    Signed-off-by: Laurent Raufaste <analogue@glop.org>

--- Game.scala
+++ Game.scala
@@ -26,6 +26,51 @@ class Game {
     
     def launch = {
         setup
-        prince.jump
+        
+        while (prince.isAlive || !gameOver) {
+            println("_" * 50)
+            println("New game")
+            println("_" * 50)
+            // The player has one in five chance to fall during the first jump
+            if (chance(1, 5)) {
+                prince jump
+                
+                // The player has one in three chance to fall in the second hole
+                if (chance(1, 3)) {
+                    prince jump
+                    
+                    // The player has one in four chance to be killed by the first enemy
+                    if (chance(1, 4)) {
+                        prince beat swordman
+                        
+                        // The player has three in ten chance to enter the castle without alerting guardians
+                        if (chance(3, 10) || chance (3, 10)) {
+                            prince enterCastle
+                            
+                            // The player has one in two chance to beat the boss
+                            if (chance(1, 2)) {
+                                prince beat boss
+                                
+                                // The player has one in three chance to be slapped by the princess
+                                if (chance(1, 3)) {
+                                    prince kiss princess
+                                    princess kiss prince
+                                    gameOver = true
+                                } else princess slap prince
+                            } else boss beat prince
+                        } else println(prince.name + " have been catched by the guards")
+                    } else swordman beat prince
+                } else {
+                    println(prince.name + " fell in the second hole")
+                    prince fall
+                }
+            } else {
+                println(prince.name + " fell in the first hole")
+                prince fall
+            }
+            
+            // Wait one second before next try
+            // Thread.sleep(1000)
+        }
     }
 }
